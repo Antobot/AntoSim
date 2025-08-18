bd5ba1df44d3e4dddf3d4b92592c1fde0caef1d4
Daniel Freer <daniel.freer@antobot.ai>
2025-08-18 11:00:04 +0800

    Launching rviz controller from description package

--- antobot_sim_bringup/launch/antobot_sim.launch.py
+++ antobot_sim_bringup/launch/antobot_sim.launch.py
@@ -106,7 +106,7 @@ def generate_launch_description():
         rviz = Node(
         package='rviz2',
         executable='rviz2',
-        arguments=['-d', os.path.join(pkg_antobot_sim_bringup, 'config', 'diff_drive.rviz')],
+        arguments=['-d', os.path.join(pkg_antobot_description, 'config', 'diff_drive.rviz')],
         condition=IfCondition(LaunchConfiguration('rviz'))
         )
     elif robot_platform == "allWheel":
